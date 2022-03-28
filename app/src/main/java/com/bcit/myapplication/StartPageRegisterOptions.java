2cc329b53056300aef907f5b71a22795a00f2632
Sean <xzhu32@my.bcit.ca>
2022-03-27 22:45:56 -0700

    Comment for StartPageRegisterOptions.java.

--- app/src/main/java/com/bcit/myapplication/StartPageRegisterOptions.java
+++ app/src/main/java/com/bcit/myapplication/StartPageRegisterOptions.java
@@ -6,8 +6,16 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Creates the StartPageRegisterOptions activity.
+ */
 public class StartPageRegisterOptions extends AppCompatActivity {
 
+    /**
+     * Creates the essential component in this activity.
+     *
+     * @param savedInstanceState a Bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +26,18 @@ public class StartPageRegisterOptions extends AppCompatActivity {
 
     }
 
+    /**
+     * Go to the register user activity by clicking the register user button.
+     */
     public void goToRegisterActivity(){
         Intent registerActivity = new Intent(this, RegisterActivity.class);
         Button registerUser = (Button) findViewById(R.id.register_user_btn);
         registerUser.setOnClickListener(view -> startActivity(registerActivity));
     }
 
+    /**
+     * Go to the register company activity by clicking the register company button.
+     */
     public void goToRegisterCompanyActivity(){
         Intent registerCompanyActivity = new Intent(this, RegisterCompany.class);
         Button registerCompany = (Button) findViewById(R.id.register_company_btn);
